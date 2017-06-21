52a2e96187ccbf41f33f2ac39d9186ddb658efc7
Rajesh Subburaman <rajesh.subburaman@iit.it>
2017-06-21 17:31:41 +0200

    Yarp dependencies removed

--- cmake/BuildDoorOpening.cmake
+++ cmake/BuildDoorOpening.cmake
@@ -2,8 +2,7 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
-find_or_build_package(paramHelp QUIET)
-find_or_build_package(OpenSoT QUIET)
+find_or_build_package(XCM QUIET)
 
 ycm_ep_helper(DoorOpening     
                                TYPE GIT
@@ -11,6 +10,6 @@ ycm_ep_helper(DoorOpening
                                REPOSITORY ADVRHumanoids/DoorOpening.git 
                                TAG master
                                COMPONENT external
-                               DEPENDS paramHelp
-                                       OpenSoT
+                               DEPENDS XCM
+                                       
 	     )
