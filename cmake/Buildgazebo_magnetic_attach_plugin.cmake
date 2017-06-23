8d5985b792bdc1bb48e69b17fdd00d894997a2a4
Enrico Mingo <enrico.mingo@iit.it>
2017-06-23 10:45:02 +0200

    Added gazebo_magnetic_attach_plugin into superbuild

---
+++ cmake/Buildgazebo_magnetic_attach_plugin.cmake
@@ -0,0 +1,15 @@
+# gazebo_magnetic_attach_plugin
+include(YCMEPHelper)
+
+find_package(roscpp QUIET)
+find_package(gazebo QUIET)
+
+ycm_ep_helper(  gazebo_magnetic_attach_plugin
+                TYPE GIT
+                STYLE GITLAB
+                REPOSITORY git@gitlab.com:xinsongyan/gazebo_magnetic_attach_plugin.git
+                TAG master
+                COMPONENT external
+                DEPENDS roscpp
+                        gazebo
+            )
