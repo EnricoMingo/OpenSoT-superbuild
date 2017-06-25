82a9e23c1adec482683118274ebc03dbc1adfae9
Luca Muratore <luca.muratore@iit.it>
2017-06-25 17:55:53 +0200

    Added possible dependency OpenSoT-lite on MiscellaneousPlugin (OpenSoTIK)

--- cmake/BuildMiscellaneousPlugins.cmake
+++ cmake/BuildMiscellaneousPlugins.cmake
@@ -3,6 +3,7 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(XCM QUIET)
+find_or_build_package(OpenSoT-lite QUIET)
 
 ycm_ep_helper(  MiscellaneousPlugins
                 TYPE GIT
@@ -11,4 +12,5 @@ ycm_ep_helper(  MiscellaneousPlugins
                 TAG master
                 COMPONENT external
 		DEPENDS XCM
+			OpenSoT-lite
              )
