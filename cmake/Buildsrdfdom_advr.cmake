2509289608ecf0f12bd3755995934363b585a646
Luca Muratore <luca.muratore@iit.it>
2017-06-27 10:51:14 +0200

    gtest_build_tests on srdfdom_advr ON by default

--- cmake/Buildsrdfdom_advr.cmake
+++ cmake/Buildsrdfdom_advr.cmake
@@ -7,4 +7,5 @@ ycm_ep_helper(  srdfdom_advr
                 REPOSITORY ADVRHumanoids/srdfdom.git
                 TAG master
                 COMPONENT external
-             )
+		CMAKE_CACHE_ARGS -Dgtest_build_tests:BOOL=ON
+		)
