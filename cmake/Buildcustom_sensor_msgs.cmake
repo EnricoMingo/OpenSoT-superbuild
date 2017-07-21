41ac2950d2c4ad86505d83e6d6caf64856381449
Enrico Mingo <enrico.mingo@iit.it>
2017-07-21 13:05:50 +0200

    removed files

--- cmake/Buildcustom_sensor_msgs.cmake
+++
@@ -1,9 +0,0 @@
-include(YCMEPHelper)
-include(FindOrBuildPackage)
-
-ycm_ep_helper(custom_sensor_msgs TYPE GIT
-                                 STYLE GITHUB
-                                 REPOSITORY ADVRHumanoids/custom_sensor_msgs.git 
-                                 TAG master
-                                 COMPONENT external
-	     )
--- cmake/Buildnetwork.cmake
+++
@@ -1,9 +0,0 @@
-# network
-include(YCMEPHelper)
-
-ycm_ep_helper(network TYPE GIT
-                      STYLE GITHUB
-                      REPOSITORY ADVRHumanoids/network-drc.git
-                      TAG master
-                      COMPONENT external)
-
