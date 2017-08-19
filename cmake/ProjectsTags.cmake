65899e7c31aae5cdb6cd98c869a4e7a71a781262
Enrico Mingo <enrico.mingo@iit.it>
2017-08-19 21:42:17 +0200

    changed version of OpenSoT

--- cmake/ProjectsTags.cmake
+++ cmake/ProjectsTags.cmake
@@ -1,64 +1,19 @@
 # Superbuild
 set(YCM_TAG 538bec08ad3)
 
-# Superbuild DRC CORE: put the SHA1 of the commit or the TAG, not the branch name.
-set(YARP_TAG ecd0040)
-set(paramHelp_TAG de9f652)
-set(GazeboYARPPlugins_TAG b843248)
-set(iDynTree_TAG f0346c1)
-set(idynutils_TAG ba98bc36)
-set(OpenSoT_TAG c46cc3b409564)
-set(GYM_TAG 825a36ed7ae)
+set(OpenSoT_TAG v2.0)
 set(fcl_TAG 54e9619bc2b)
 
-# COMAN
-set(comaninterface_TAG VVV15)
-set(comanapp_TAG VVV15)
-set(comanmod_TAG VVV15)
 
 # CAPSULE: put the SHA1 of the commit or the TAG, not the branch name.
 set(roboptim-core_TAG 9f95efdc6) # need masters on 16.04
 set(roboptim-core-plugin-ipopt_TAG 2e04f69e497) # needs master on 16.04
 set(roboptim-capsule_TAG 7ac32a4c) # needs master on 16.04
 
-# old modules
-set(drc_door_TAG 89f229f51)
-set(arms_ik_TAG 5d83c506a1)
-set(locoman_service_1_TAG 572d134df5)
-set(locoman_service_2_TAG e810d67d9cb)
-set(locoman_utils_TAG 89cd9b83582a)
 
 # utils: put the SHA1 of the commit or the TAG, not the branch name.
 #set(drc_shared_TAG 8289f4fed1dd)
 set(trajectory_generator_TAG 332b984362)
 
-# ROS publisher: put the SHA1 of the commit or the TAG, not the branch name.
-set(robot_state_publisher_ext_TAG 90ffac07f)
-set(yarp_ros_joint_state_publisher_TAG 101ddfb3c)
 
-# XBot Core libraries
-
-set(ADVR_shared_TAG f9e8f0d8196f50a)
-
-#set(sharedlibpp_TAG v1.0)
-#set(srdfdom_advr_TAG 67bfe684b3703276b)
-#set(XBotCoreModel_TAG 9be02db2583)
-#set(XBotCore-interfaces_TAG e1009fb5921a737)
-#set(XBotInterface_TAG ef58f663676d0749)
-#set(XCM_TAG 9c9d211a4f39a304)
-
-#set(RobotInterfaceDUMMY_TAG 72c9400323)
-#set(RobotInterfaceXBotRT_TAG 7057c412d28) 
-#set(RobotInterfaceYARP_TAG c1ae076cf4cc)
-#set(RobotInterfaceROS_TAG 55ab6536aceff0fe)
-
-#set(ModelInterfaceRBDL_TAG a45209773ebdee)
-
-#set(XBotCore_TAG 885eea96b56c126)
-
-#set(GazeboXBotPlugin_TAG cca4168f6b9d2c227dae7)
-
-# Robots
-#set(iit-bigman-ros-pkg_TAG 2b9e47b18f24e0)
-#set(centauro-simulator_TAG bc52885a985ce750f)
 
