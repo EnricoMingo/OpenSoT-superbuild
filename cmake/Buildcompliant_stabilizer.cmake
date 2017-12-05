85a5dc9564b249288d0bc693a6be2f75fa08368e
Enrico Mingo <enrico.mingo@iit.it>
2017-12-05 10:43:14 +0100

    Added two more packages: compliant_stabilizer and mpcqp_walking

---
+++ cmake/Buildcompliant_stabilizer.cmake
@@ -0,0 +1,12 @@
+# compliant_stabilizer
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+
+ycm_ep_helper(  compliant_stabilizer
+                TYPE GIT
+                STYLE GITHUB
+                REPOSITORY ADVRHumanoids/compliant_stabilizer
+                TAG master
+                COMPONENT external
+             )
---
+++ cmake/Buildmpcqp_walking.cmake
@@ -0,0 +1,12 @@
+# mpcqp_walking
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+
+ycm_ep_helper(  mpcqp_walking
+                TYPE GIT
+                STYLE GITHUB
+                REPOSITORY ADVRHumanoids/mpcqp_walking
+                TAG master
+                COMPONENT external
+             )
