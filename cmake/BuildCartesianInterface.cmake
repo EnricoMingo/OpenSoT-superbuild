1a6b48747d39db3f3ceb4b5dfa92b54e7c3a3ef3
EnricoMingo <enricomingo@gmail.com>
2018-03-13 18:17:49 +0100

    fixed wrong dep

--- cmake/BuildCartesianInterface.cmake
+++ cmake/BuildCartesianInterface.cmake
@@ -3,7 +3,7 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(XBotInterface QUIET)
-find_or_build_package(OpenSoT-lite QUIET)
+find_or_build_package(OpenSoT QUIET)
 
 ycm_ep_helper(CartesianInterface
               TYPE GIT
@@ -11,5 +11,5 @@ ycm_ep_helper(CartesianInterface
               REPOSITORY ADVRHumanoids/CartesianInterface.git
               TAG master
               COMPONENT external
-	      DEPENDS OpenSoT-lite XBotInterface
+	      DEPENDS OpenSoT XBotInterface
              )
