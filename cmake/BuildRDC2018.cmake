469d643734b152020628bdd85d4d0fb0509cfee1
EnricoMingo <enricomingo@gmail.com>
2018-06-07 13:27:39 +0200

    Added RDC2018 to superbuild

---
+++ cmake/BuildRDC2018.cmake
@@ -0,0 +1,12 @@
+# RDC2018
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+ycm_ep_helper(  RDC2018
+                TYPE GIT
+                STYLE GITHUB
+                REPOSITORY ADVRHumanoids/RDC2018.git
+                TAG master
+                COMPONENT external
+                INSTALL_COMMAND ""
+            )
