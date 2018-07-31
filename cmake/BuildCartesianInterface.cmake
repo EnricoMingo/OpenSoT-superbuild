6041f78723bcbfd50da43c8fce88855fa103b7ca
EnricoMingo <enricomingo@gmail.com>
2018-07-31 12:39:06 +0200

    Added dependency to reflexxes in cartesina interface

--- cmake/BuildCartesianInterface.cmake
+++ cmake/BuildCartesianInterface.cmake
@@ -4,6 +4,7 @@ include(FindOrBuildPackage)
 
 find_or_build_package(XBotInterface QUIET)
 find_or_build_package(OpenSoT QUIET)
+find_or_build_package(RMLTypeII QUIET)
 find_or_build_package(RobotInterfaceROS QUIET)
 
 ycm_ep_helper(CartesianInterface
@@ -12,5 +13,5 @@ ycm_ep_helper(CartesianInterface
               REPOSITORY ADVRHumanoids/CartesianInterface.git
               TAG master
               COMPONENT external
-	      DEPENDS OpenSoT XBotInterface RobotInterfaceROS
+	      DEPENDS OpenSoT XBotInterface RobotInterfaceROS RMLTypeII
              )
