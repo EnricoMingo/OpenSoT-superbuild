a8fda7e505b63f4dc204db6b15f160a8d80c9b38
EnricoMingo <enricomingo@gmail.com>
2018-08-06 19:50:16 +0200

    fix

--- cmake/BuildXBotCore.cmake
+++ cmake/BuildXBotCore.cmake
@@ -2,23 +2,18 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
-find_or_build_package(XCM QUIET)
-
-find_or_build_package(soem_advr QUIET)
-find_or_build_package(walkman-master-ecat QUIET)
-find_or_build_package(ec_master_test QUIET)
+find_or_build_package(XBotInterface QUIET)
+find_or_build_package(RobotInterfaceROS QUIET)
 
 ycm_ep_helper(  XBotCore
                 TYPE GIT
                 STYLE GITHUB
                 REPOSITORY ADVRHumanoids/xbotcore.git
-                TAG centauro_bonn_review_2016
+                TAG master
                 COMPONENT external
-		DEPENDS XCM
-			soem_advr
-			walkman-master-ecat
-			ec_master_test
+		DEPENDS XBotInterface
+			RobotInterfaceROS
 		CMAKE_CACHE_ARGS -DBUILD_SHARED_LIBS:BOOL=ON
-                                 -DENABLE_XENO:BOOL=ON
-
+				 -DBUILD_examples:BOOL=ON
+                                 
             )
